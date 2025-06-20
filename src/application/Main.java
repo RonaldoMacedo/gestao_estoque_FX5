8a0132268b181b08d77dd272dc5de4d2d9ef28cb
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-20 10:23:55 -0300

    Tela sobre + alerts + constraints

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,12 +10,15 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/TelaPrincipal.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
@@ -30,5 +33,9 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 }
---
+++ src/gui/TelaPrincipalController.java
@@ -0,0 +1,87 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class TelaPrincipalController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//**********************************************************************
+	
+	@FXML
+	private MenuItem menuItemNovoProduto;
+	
+	@FXML
+	public void onMenuItemNovoProdutoAction() {
+		System.out.println("Novo produto");
+	}
+	
+	//**********************************************************************
+	
+	@FXML
+	private MenuItem menuItemNovoItem;
+	
+	@FXML
+	public void onMenuItemNovoItemAction() {
+		System.out.println("Novo item");
+	}
+	
+	//***********************************************************************
+	
+	@FXML
+	private MenuItem menuItemNovoFornecedor;
+	
+	@FXML
+	public void onMenuItemNovoFornecedorAction() {
+		System.out.println("Novo fornecedor");
+	}
+	
+	//***********************************************************************
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		loadView("/gui/Sobre.fxml");
+	}
+	
+	//***********************************************************************
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox telaPrincipalVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = telaPrincipalVBox.getChildren().get(0);
+			telaPrincipalVBox.getChildren().clear();
+			telaPrincipalVBox.getChildren().add(mainMenu);
+			telaPrincipalVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}catch(IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+}
