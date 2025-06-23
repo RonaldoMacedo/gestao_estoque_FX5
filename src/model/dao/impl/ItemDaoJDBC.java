80bf9ff477aa489f5acb918f16233b4987950dca
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-23 15:40:59 -0300

    ProdutoDaoJDBC, ItemDaoJDBC e daoFactory

---
+++ src/model/dao/impl/ItemDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.ItemDao;
+import model.entities.Item;
+
+public class ItemDaoJDBC implements ItemDao {
+
+	@Override
+	public void insert(Item obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Item obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteByid(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Item findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
