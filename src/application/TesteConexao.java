b9fced0f87e3d819206d000ec39dd0e8b081727a
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-23 09:52:50 -0300

    Recuperando dados do banco

--- src/application/TesteConexao.java
+++ src/application/TesteConexao.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,8 +11,29 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("select * from produto");
+			while(rs.next()) {
+				System.out.println(rs.getInt("id_produto") + " " +  rs.getString("descricao_interna") + " " + rs.getString("grupo")
+						+ " " + rs.getString("situacao") + " " + rs.getInt("saldo"));
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 
 	}
 
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -46,6 +48,28 @@ public class DB {
 				conn.close();
 			}
 			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	// método para fechar o objeto Statement
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	// método para fechar o objeto ResultSet
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
