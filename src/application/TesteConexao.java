db765a8d3969370f49b8f91d3de45025194cb6d0
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-23 11:18:25 -0300

    Atualizando dados

--- src/application/TesteConexao.java
+++ src/application/TesteConexao.java
@@ -1,13 +1,8 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -15,47 +10,29 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement ps = null;
 		
 		try {
 			conn = DB.getConnection();
-			ps = conn.prepareStatement("insert into produto " 
-					+ "(descricao_interna, data_cadastro, grupo, situacao) " 
-					+ "values " 
-					+ "(?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			ps.setString(1, "Colesterol HDL");
-			ps.setDate(2, new Date(sdf.parse("23/06/2025").getTime()));
-			ps.setString(3, "Tecnica");
-			ps.setString(4, "Ativo");
+			ps = conn.prepareStatement("update produto "
+					+ "set situacao = ? "
+					+ "where "
+					+ "(id_produto = ?)");
+			ps.setString(1, "ativo");
+			ps.setInt(2, 184);
 			
-			int rowsAffect = ps.executeUpdate();
+			int rowsAffected = ps.executeUpdate();
 			
-			if(rowsAffect > 0) {
-				ResultSet rs = ps.getGeneratedKeys();
-				while(rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		catch(ParseException e) {
 			e.printStackTrace();
 		}
 		finally {
 			DB.closeStatement(ps);
 			DB.closeConnection();
-			
 		}
-
 	}
 
 }
