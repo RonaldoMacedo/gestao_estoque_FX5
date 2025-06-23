c205cc635bc4764c64e4b437fcf45386f730b376
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-23 10:54:08 -0300

    Inserindo dados recuperando o id

--- src/application/TesteConexao.java
+++ src/application/TesteConexao.java
@@ -3,7 +3,9 @@ package application;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -19,15 +21,28 @@ public class TesteConexao {
 		
 		try {
 			conn = DB.getConnection();
-			ps = conn.prepareStatement("insert into produto " + "(descricao_interna, data_cadastro, grupo, situacao) " + "values " + "(?, ?, ?, ?)");
-			ps.setString(1, "Agulha 25x7 normal");
+			ps = conn.prepareStatement("insert into produto " 
+					+ "(descricao_interna, data_cadastro, grupo, situacao) " 
+					+ "values " 
+					+ "(?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, "Colesterol HDL");
 			ps.setDate(2, new Date(sdf.parse("23/06/2025").getTime()));
-			ps.setString(3, "Coleta");
+			ps.setString(3, "Tecnica");
 			ps.setString(4, "Ativo");
 			
 			int rowsAffect = ps.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffect);
+			if(rowsAffect > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
