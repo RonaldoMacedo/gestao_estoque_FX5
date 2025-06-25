1b3f196f4916012ae9c701a7e3c21f44d92a42c5
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-25 11:56:08 -0300

    Chamando o Productdao para acessar o banco de dados

--- src/model/services/ProductService.java
+++ src/model/services/ProductService.java
@@ -1,20 +1,18 @@
 package model.services;
 
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.ProductDao;
 import model.entities.Product;
-import model.enums.Grupo;
-import model.enums.Situacao;
 
 public class ProductService {
 	
+	private ProductDao dao = DaoFactory.createProductDao();
+	
 	// Mock de dados
 	public List<Product> findAll(){
-		List<Product> list = new ArrayList<>();
-		list.add(new Product(1, "Luva", new Date(11111), Grupo.Coleta, Situacao.Ativo, null, null));
-		return list;
+		return dao.findAll();
 	}
 
 }
