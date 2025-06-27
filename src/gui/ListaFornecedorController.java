a5af1a8fdfed58e457b4386426e6924f368719be
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-27 10:58:45 -0300

    Formulário de cadastro de fornecedor

--- src/gui/ListaFornecedorController.java
+++ src/gui/ListaFornecedorController.java
@@ -1,19 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Fornecedor;
 import model.enums.Situacao;
@@ -85,8 +94,9 @@ public class ListaFornecedorController implements Initializable {
 	private Button btNovo;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Novo fornecedor");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/FornecedorForm.fxml", parentStage);
 	}
 	
 	//************************************************************************************************************************************************************
@@ -101,4 +111,21 @@ public class ListaFornecedorController implements Initializable {
 	
 	//************************************************************************************************************************************************************
 
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Digite os dados do fornecedor");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
 }
