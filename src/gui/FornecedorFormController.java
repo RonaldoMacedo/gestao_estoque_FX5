6a305489a07a8d1e852ed26b5be81df84cdbff31
RonaldoMacedo <ronni29_100@hotmail.com>
2025-06-27 11:14:27 -0300

    FornecedorForm Controller

---
+++ src/gui/FornecedorFormController.java
@@ -0,0 +1,78 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FornecedorFormController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtIdFornecedor);
+		Constraints.setTextFieldMaxLength(txtRazaoSocial, 150);
+		Constraints.setTextFieldMaxLength(txtApelido, 60);
+		Constraints.setTextFieldMaxLength(txtCNPJ, 18);
+	}
+	
+	//*************************************************************************************************************************************************************
+	
+	@FXML
+	private TextField txtIdFornecedor;
+	
+	@FXML
+	private TextField txtRazaoSocial;
+	
+	@FXML
+	private TextField txtApelido;
+	
+	@FXML
+	private TextField txtCNPJ;
+	
+	@FXML
+	private TextField txtDataCadastro;
+	
+	@FXML
+	private TextField txtSituacao;
+	
+	//*************************************************************************************************************************************************************
+
+	@FXML
+	private Label lblErroRazaoSocial;
+	
+	@FXML
+	private Label lblErroApelido;
+	
+	@FXML
+	private Label lblErroCNPJ;
+	
+	//*************************************************************************************************************************************************************
+	
+	@FXML
+	private Button btSalvar;
+	
+	@FXML
+	public void onBtSalvarAction() {
+		System.out.println("Salvar");
+	}
+	
+	@FXML
+	private Button btCancelar;
+	
+	@FXML
+	public void onBtCancelarAction() {
+		System.out.println("Cancelar");
+	}
+	
+	//*************************************************************************************************************************************************************
+}
