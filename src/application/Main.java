4e59f896d72ca9085dbda17dc1456ab72ae522f6
RonaldoMacedo <ronni29_100@hotmail.com>
2025-08-22 08:33:22 -0300

    Renomeando tela principal para mainview

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/TelaPrincipal.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
 			mainScene = new Scene(scrollPane);
 			
